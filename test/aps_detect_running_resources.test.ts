8f0dc026f7c22872fca34d4ce6d71f0f805d1b2a
Allam <jallam@842f573b4463.ant.amazon.com>
2025-12-09 10:39:55 +0300

    Initial commit

---
+++ test/aps_detect_running_resources.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApsDetectRunningResources from '../lib/aps_detect_running_resources-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aps_detect_running_resources-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApsDetectRunningResources.ApsDetectRunningResourcesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
